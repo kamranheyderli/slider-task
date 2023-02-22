803ae828ad8bc557020947af50ee60ae87365abd
kamranfrontend <kamranheyderli049@gmail.com>
2023-02-23 00:59:12 +0400

    slider/task

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+const slides = document.querySelectorAll('.slide')
+
+for (const slide of slides) {
+    slide.addEventListener('click', () => {
+      clearActiveClasses()
+      
+      
+      slide.classList.add('active')
+    })
+}
+
+function clearActiveClasses() {
+    slides. forEach((slide) => {
+        slide. classList. remove('active')
+    })
+}
